7f9394ded963ebe6367b30a6627c6f03b247f4c1
Andrew Mcveigh <me@andrewmcveigh.com>
2012-06-22 12:04:20 +0200

    converted to one data-source for all formats

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.andrewmcveigh.curriculum-vitae "0.0.1-SNAPSHOT"
+            :description "TODO: add summary of your project"
+            :dependencies [[org.clojure/clojure "1.4.0"]
+                           [hiccup "1.0.0"]]
+            :profiles {:user {:plugins [[lein-tarsier "0.9.1"]]}})
---
+++ src/com/andrewmcveigh/curriculum_vitae/core.clj
@@ -0,0 +1,75 @@
+(ns com.andrewmcveigh.curriculum-vitae.core
+  (:use
+    [clojure pprint]
+    [clojure.java shell]
+    [hiccup core])
+  (:require
+    [com.andrewmcveigh.curriculum-vitae [info :as info]]
+    [clojure [string :as string :only [replace]]]))
+
+(defn render-keyword [k syntax]
+  (case k
+    :h1 (str "\n" (case syntax :org "* "   :markdown "# "))
+    :h2 (str "\n" (case syntax :org "** "  :markdown "## "))
+    :h3 (str "\n" (case syntax :org "*** " :markdown "### "))
+    :li "\n- "
+    :p  "\n"
+    nil))
+
+(defn cv->syntax [element syntax]
+  (apply str
+         (cond 
+           (coll? element)    (map #(cv->syntax % syntax) element)
+           (keyword? element) (render-keyword element syntax)
+           :default           element)))
+
+(defn render-brace [cmd elem]
+  (str (apply str \\ cmd "{" elem) "}\n"))
+
+(defn render-tex [elems]
+  (case (first elems)
+    :doc   {:begin (render-brace "begin" "document")
+            :end   (render-brace "end" "document")}
+    :title "\\maketitle\n"
+    :h1    (render-brace "section*" (rest elems))
+    :h2    (render-brace "subsection*" (rest elems))
+    :h3    (str (apply str "\\subsubsection*{\\emph{" (rest elems)) "}}\n")
+    :ul    {:begin (render-brace "begin" "itemize")
+            :end   (render-brace "end" "itemize")}
+    :li    (str (apply str "\\item " (rest elems)) "\n")
+    :p     (str (apply str (rest elems)) "\n")
+    nil))
+
+(defn escape-tex-str [s]
+  (string/replace
+    (string/replace s #"([&%$#_\{\}~^\\])" "\\\\$1")
+    #"\"([^\"]*)\"" "``$1''"))
+
+(defn cv->tex [element]
+  (let [level (cond 
+                (coll? element)   (let [elems (map #(if (string? %)
+                                                      (escape-tex-str %)
+                                                      %)
+                                                   (filter #(not (coll? %))
+                                                           element))
+                                        inner (filter coll? element)]
+                                    (apply list
+                                           (render-tex elems)
+                                           (map cv->tex inner)))
+                (string? element) (escape-tex-str element)
+                :default          element)]
+    (if (:begin (first level))
+      (str (:begin (first level)) (apply str (rest level)) (:end (first level)))
+      (apply str level))))
+
+(defn tex->pdf []
+  (do
+    (spit "cv.tex"
+          (str
+            (slurp "tex-header.tex")
+            (cv->tex [:doc [:title] info/cv])))
+    (sh "pdflatex" "-interaction=batchmode" "cv.tex")))
+
+(spit "README.md" (cv->syntax info/cv :markdown))
+(spit "cv.org" (cv->syntax info/cv :org))
+(tex->pdf)
