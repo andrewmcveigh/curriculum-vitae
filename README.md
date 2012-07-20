f7f5bee8c82d8709b6a6dfc87f3ea42c729ebcb2
Andrew Mcveigh <andrew.mcveigh@interelgroup.com>
2012-07-20 12:47:21 +0200

    updated langs/software

--- README.md
+++ README.md
@@ -21,28 +21,43 @@
 # Technical Summary
 
 ## Languages / Frameworks
-.NET, ASP.NET(MVC), AppleScript, Bash, C#, CSS, Clojure, Cocoa, Django, HAML, HTML, Java, Javascript, Objective-C, Python, SASS, SQL, Silverlight, Tex, jQuery
+ASP.NET(MVC), AppleScript, Bash, C#, CSS, Clojure, ClojureScript, Cocoa, Django, HAML, HTML, Java, Javascript, MS.NET, Objective-C, Python, SASS, SQL, Silverlight, Tex, jQuery
 
 ## Operating Systems, Software & Technologies
-Apache, Eclipse, Flash, Git, IIS, Illustrator, Lighttpd, Linux, MS Exchange, Maven, Mercurial, MongoDB, MySQL, Netbeans, Nginx, OS X, Photoshop, PostgreSQL, Puppet, SQL Server, SQLite, Subversion, VMWare, Vim, Visual Studio, Windows (NT/2000/XP/Sever/7)
+Apache, Eclipse, Flash, Git, IIS, Illustrator, Jenkins, Leiningen, Lighttpd, Linux, MS Exchange, Maven, Mercurial, MongoDB, MySQL, Netbeans, Nginx, OS X, Photoshop, PostgreSQL, Puppet, SQL Server, SQLite, Subversion, VMWare, Vim, Visual Studio, Windows (NT/2000/XP/Server/7)
 
 # Experience
 
 ## Software Developer
 
 ### Interel Group, Brussels  [2008 - Present]
-Hired on an IT helpdesk/software development student placement, later offered a full-time position as a software developer. Now responsible for the design, development, and maintenance of all the group's software infrastructure.
-- Created/Deployed "Webinars" system using "BigBlueButton" & a custom scheduling webapp. [Java/Clojure]
-- Delivered a customised implementation of an "XWiki Farm". [Java/J2EE/Clojure]
-- Client Wins: CRUD webapp with Javascript image editor [Clojure/Javascript/jQuery]
-- Helpdesk email monitor: Monitors an Exchange mailbox for emails, adds an entry in the ticketing system. [Clojure/SOAP/Exchange Web Services API (Java)]
-- Helpdesk ticketing/inventory webapp. [C#/ASP.NET MVC/Javascript/jQuery/T-SQL]
-- Inventory tracking: Periodically scans the network for changes, integrated with the Intranet, and the ticketing system. [C#/WMI/T-SQL]
-- TAPI Monitor: Windows Service, monitoring a VOIP extention for incoming calls, looks up caller from called id, logs in ticketing system. [TAPI/C#/.NET/WCF]
-- Maintenance of the company Intranet [VB.NET/C#/Javascript]
-- Building reports on various existing databases. [C#/Crystal Reports/Clojure/Incanter]
+Hired on an IT helpdesk/software development student placement,
+          later offered a full-time position as a software developer. Now
+          responsible for the design, development, and maintenance of all the
+          group's software infrastructure.
+- Created/Deployed "Webinars" system using "BigBlueButton" & a
+            custom scheduling webapp. [Java/Clojure]
+- Delivered a customised implementation of an "XWiki Farm".
+            [Java/J2EE/Clojure]
+- Client Wins: CRUD webapp with Javascript image editor
+            [Clojure/Javascript/jQuery]
+- Helpdesk email monitor: Monitors an Exchange mailbox for emails,
+            adds an entry in the ticketing system.
+            [Clojure/SOAP/Exchange Web Services API (Java)]
+- Helpdesk ticketing/inventory webapp.
+            [C#/ASP.NET MVC/Javascript/jQuery/T-SQL]
+- Inventory tracking: Periodically scans the network for changes,
+            integrated with the Intranet, and the ticketing system.
+        [C#/WMI/T-SQL]
+- TAPI Monitor: Windows Service, monitoring a VOIP extention for
+            incoming calls, looks up caller from called id, logs in ticketing
+            system. [TAPI/C#/.NET/WCF]
+- Maintenance of the company Intranet [C#/Javascript]
+- Building reports on various existing databases.
+            [C#/Crystal Reports/Clojure/Incanter]
 - Financial report building [Business Objects/Maconomy AnalytiX]
-- Distributed configuration managment of various *NIX based servers. [Puppet]
+- Distributed configuration managment of various *NIX based servers.
+            [Puppet]
 - Implemented Continuous integration system [Jenkins]
 - Implemented Maven build system [Maven/Nexus]
 - Various scripting/sysadmin tasks [Bash/Python/*NIX]
@@ -50,19 +65,25 @@ Hired on an IT helpdesk/software development student placement, later offered a 
 ## Sales Assistant
 
 ### Gamestation, Huddersfield [2005 - 2006]
-Duties/responsibilities: staff training, stock control, cashing up, banking, administration, and customer service.
+Duties/responsibilities: staff training, stock control, cashing up,
+          banking, administration, and customer service.
 
 ## Shift Manager
 
 ### Allied Leisure, Huddersfield [2001 - 2004]
-Duties/responsibilities: staff management, staff training, stock control, ordering, cashing up, banking, loss control, administration, and customer service.
+Duties/responsibilities: staff management, staff training, stock
+          control, ordering, cashing up, banking, loss control, administration,
+          and customer service.
 
 ## Insurance Assistant
 
 ### Fartown Insurance, Huddersfield [2000 - 2001]
-Duties/responsibilities: customer quotations, renewals, developing and maintaining the company website, other general administration duties.
+Duties/responsibilities: customer quotations, renewals, developing
+          and maintaining the company website, other general administration
+          duties.
 
 ## Computer Technician
 
 ### Easy Computers, Huddersfield [1998 - 2000]
-Duties/responsibilities: build, repair, update and install home computers, on-site network installation and maintenance.
+Duties/responsibilities: build, repair, update and install home
+          computers, on-site network installation and maintenance.
--- src/com/andrewmcveigh/curriculum_vitae/info.clj
+++ src/com/andrewmcveigh/curriculum_vitae/info.clj
@@ -20,50 +20,71 @@
            (interpose
              ", "
              (sort
-               ["Clojure" "C#" "Java" "Javascript" "Python" "Bash"
-                "SQL" "HTML" "HAML" "CSS" "SASS" "ASP.NET(MVC)"
-                ".NET" "Silverlight" "jQuery" "Django" "Cocoa"
-                "Objective-C" "AppleScript" "Tex"])))]]
+               ["Clojure" "ClojureScript" "C#" "Java" "Javascript" "Python"
+                "Bash" "SQL" "HTML" "HAML" "CSS" "SASS" "ASP.NET(MVC)" "MS.NET"
+                "Silverlight" "jQuery" "Django" "Cocoa" "Objective-C"
+                "AppleScript" "Tex"])))]]
     [:h2 "Operating Systems, Software & Technologies"
      [:p (apply 
           str
            (interpose
              ", "
              (sort
-               ["Linux" "OS X" "Windows (NT/2000/XP/Sever/7)"
-                "Vim" "Visual Studio" "Netbeans" "Eclipse" "Git"
-                "Mercurial" "Subversion" "Maven" "Puppet" "Apache"
-                "IIS" "Lighttpd" "Nginx" "MongoDB" "SQLite"
-                "MySQL" "PostgreSQL" "SQL Server" "Flash"
-                "Photoshop" "Illustrator" "VMWare" "MS Exchange"])))]]]
+               ["Linux" "OS X" "Windows (NT/2000/XP/Server/7)" "Vim"
+                "Visual Studio" "Netbeans" "Eclipse" "Git" "Mercurial"
+                "Subversion" "Maven" "Leiningen" "Puppet" "Apache" "Jenkins"
+                "IIS" "Lighttpd" "Nginx" "MongoDB" "SQLite" "MySQL"
+                "PostgreSQL" "SQL Server" "Flash" "Photoshop" "Illustrator"
+                "VMWare" "MS Exchange"])))]]]
    [:h1 "Experience"
     [:h2 "Software Developer"
      [:h3 "Interel Group, Brussels  [2008 - Present]"
-      [:p "Hired on an IT helpdesk/software development student placement, later offered a full-time position as a software developer. Now responsible for the design, development, and maintenance of all the group's software infrastructure."]
+      [:p "Hired on an IT helpdesk/software development student placement,
+          later offered a full-time position as a software developer. Now
+          responsible for the design, development, and maintenance of all the
+          group's software infrastructure."]
       [:ul
-       [:li "Created/Deployed \"Webinars\" system using \"BigBlueButton\" & a custom scheduling webapp. [Java/Clojure]"]
-       [:li "Delivered a customised implementation of an \"XWiki Farm\". [Java/J2EE/Clojure]"]
-       [:li "Client Wins: CRUD webapp with Javascript image editor [Clojure/Javascript/jQuery]"]
-       [:li "Helpdesk email monitor: Monitors an Exchange mailbox for emails, adds an entry in the ticketing system. [Clojure/SOAP/Exchange Web Services API (Java)]"]
-       [:li "Helpdesk ticketing/inventory webapp. [C#/ASP.NET MVC/Javascript/jQuery/T-SQL]"]
-       [:li "Inventory tracking: Periodically scans the network for changes, integrated with the Intranet, and the ticketing system. [C#/WMI/T-SQL]"]
-       [:li "TAPI Monitor: Windows Service, monitoring a VOIP extention for incoming calls, looks up caller from called id, logs in ticketing system. [TAPI/C#/.NET/WCF]"]
-       [:li "Maintenance of the company Intranet [VB.NET/C#/Javascript]"]
-       [:li "Building reports on various existing databases. [C#/Crystal Reports/Clojure/Incanter]"]
+       [:li "Created/Deployed \"Webinars\" system using \"BigBlueButton\" & a
+            custom scheduling webapp. [Java/Clojure]"]
+       [:li "Delivered a customised implementation of an \"XWiki Farm\".
+            [Java/J2EE/Clojure]"]
+       [:li "Client Wins: CRUD webapp with Javascript image editor
+            [Clojure/Javascript/jQuery]"]
+       [:li "Helpdesk email monitor: Monitors an Exchange mailbox for emails,
+            adds an entry in the ticketing system.
+            [Clojure/SOAP/Exchange Web Services API (Java)]"]
+       [:li "Helpdesk ticketing/inventory webapp.
+            [C#/ASP.NET MVC/Javascript/jQuery/T-SQL]"]
+       [:li "Inventory tracking: Periodically scans the network for changes,
+            integrated with the Intranet, and the ticketing system.
+        [C#/WMI/T-SQL]"]
+       [:li "TAPI Monitor: Windows Service, monitoring a VOIP extention for
+            incoming calls, looks up caller from called id, logs in ticketing
+            system. [TAPI/C#/.NET/WCF]"]
+       [:li "Maintenance of the company Intranet [C#/Javascript]"]
+       [:li "Building reports on various existing databases.
+            [C#/Crystal Reports/Clojure/Incanter]"]
        [:li "Financial report building [Business Objects/Maconomy AnalytiX]"]
-       [:li "Distributed configuration managment of various *NIX based servers. [Puppet]"]
+       [:li "Distributed configuration managment of various *NIX based servers.
+            [Puppet]"]
        [:li "Implemented Continuous integration system [Jenkins]"]
        [:li "Implemented Maven build system [Maven/Nexus]"]
        [:li "Various scripting/sysadmin tasks [Bash/Python/*NIX]"]]]]
     [:h2 "Sales Assistant"
      [:h3 "Gamestation, Huddersfield [2005 - 2006]"
-      [:p "Duties/responsibilities: staff training, stock control, cashing up, banking, administration, and customer service."]]]
+      [:p "Duties/responsibilities: staff training, stock control, cashing up,
+          banking, administration, and customer service."]]]
     [:h2 "Shift Manager"
      [:h3 "Allied Leisure, Huddersfield [2001 - 2004]"
-      [:p "Duties/responsibilities: staff management, staff training, stock control, ordering, cashing up, banking, loss control, administration, and customer service."]]]
+      [:p "Duties/responsibilities: staff management, staff training, stock
+          control, ordering, cashing up, banking, loss control, administration,
+          and customer service."]]]
     [:h2 "Insurance Assistant"
      [:h3 "Fartown Insurance, Huddersfield [2000 - 2001]"
-      [:p "Duties/responsibilities: customer quotations, renewals, developing and maintaining the company website, other general administration duties."]]]
+      [:p "Duties/responsibilities: customer quotations, renewals, developing
+          and maintaining the company website, other general administration
+          duties."]]]
     [:h2 "Computer Technician"
      [:h3 "Easy Computers, Huddersfield [1998 - 2000]"
-      [:p "Duties/responsibilities: build, repair, update and install home computers, on-site network installation and maintenance."]]]]])
+      [:p "Duties/responsibilities: build, repair, update and install home
+          computers, on-site network installation and maintenance."]]]]])
